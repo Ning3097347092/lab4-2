6cc715ce5d3497334ef7ae37a82700ba71e863c0
Ning <3097347092@qq.com>
2024-03-27 20:29:09 +0800

    2st commit,only main.cpp modified!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <QCoreApplication>
 #include <QDebug>
 
+void  BubbleSort(QVector<int> & v)      // 冒泡排序法
+{
+    for(int i = 0 ; i < v.size();i++)
+    {   bool isSorted = true;
+        for(int j = 0; j <v.size()-i-1;j++)
+        {   if(v[j+1] <v[j])
+            {   isSorted = false;
+                int temp = v[j];
+                v[j] = v[j+1];
+                v[j+1] = temp;
+            }
+        }
+        if(isSorted)    break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -8,11 +24,12 @@ int main(int argc, char *argv[])
     QVector<int> vec(10);                // 定义一个连续数组,10个int的空间
 
     for(int i=0; i < 10; i++)
-        vec [ i ] = rand() % 100;           //  使用随机函数赋值到数组中
+        vec[i] =  rand() % 100;           //  使用随机函数赋值到数组中
+
     qDebug() << "原始数列："<< vec;       // 输出数组
 
-    std::sort(vec.begin(),vec.end());       // 使用Qt的排序函数对数组进行排序
-
+    BubbleSort(vec);
+    //qSort(vec.begin(),vec.end());       // 使用Qt的排序函数对数组进行排序
     qDebug() << "已排序数列："<< vec;     // 再次输出已排序的数组
 
     return a.exec();

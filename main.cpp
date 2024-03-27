a98eb912363d431962a53f75601d1e70e796b5df
Ning <3097347092@qq.com>
2024-03-27 20:58:15 +0800

    3st commit!

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
 #include <QCoreApplication>
 #include <QDebug>
-
-void  BubbleSort(QVector<int> & v)      // 冒泡排序法
-{
-    for(int i = 0 ; i < v.size();i++)
-    {   bool isSorted = true;
-        for(int j = 0; j <v.size()-i-1;j++)
-        {   if(v[j+1] <v[j])
-            {   isSorted = false;
-                int temp = v[j];
-                v[j] = v[j+1];
-                v[j+1] = temp;
-            }
-        }
-        if(isSorted)    break;
-    }
-}
+#include <bubblesort.h>
 
 int main(int argc, char *argv[])
 {
